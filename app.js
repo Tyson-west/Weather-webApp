15eebf533e1f9529defe6b192c4b3068ea65d406
Tyson-west <dikeweststrong@gmail.com>
2023-08-22 13:24:42 +0100

    made the site responsive

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const https = require("https")
 const bodyParser = require('body-parser')
@@ -43,39 +44,6 @@ app.post("/weatherReport", function (req, res) {
         })
     })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(process.env.PORT || 3000,function (params) {
